03735cb0436c1582ab1990f5b0dab680fd9791d9
Ddedalus <h.beres@hotmail.com>
2019-07-27 16:19:57 +0100

    Logic for parsing the .json

--- copy_from_list.ps1
+++ copy_from_list.ps1
@@ -16,24 +16,59 @@ $file = Get-Content -Path "$List" -Raw
 $cfg =  ConvertFrom-Json -InputObject $file
 $cfg = psobj_to_hashmap($cfg)
 
-# foreach ($item in $cfg) {
-#     $localPath = $item.local
-#     $storePath = $item.store
-#     if (-Not(Test-Path "$localPath")){
-#         Write-Output "Can't find local: $localPath"
-#         continue
-#     }
-#     if (-Not(Test-Path "$storePath")){
-#         Write-Output "Can't find store: $storePath"
-#         continue
-#     }
+foreach ($item in $cfg) {
+    $localPath = $item["local"]
+    $storePath = $item["store"]
+    if (-Not(Test-Path "$localPath")){
+        Write-Output "Can't find local: $localPath"
+        continue
+    }
+    if (-Not(Test-Path "$storePath")){
+        Write-Output "Can't find store: $storePath"
+        continue
+    }
 
-#     foreach ($glob in $item.dirs) {
-#         $fromPath = Join-Path $storePath "$($glob.path)"
-#         $toPath = Join-Path $localPath "$($glob.path)"
-#         Write-Output "Copying from $fromPath to $toPath"
-#     }
-# }
+    # initialize exclude lists with empty lists which will evaluate to false if
+    # asked for in an if statement anyway...
+    if (-Not($item["exclude_file"])) {
+        $item["exclude_file"] =  @()
+    }
+    if (-Not($item["exclude_dir"])) {
+        $item["exclude_dir"] = @()
+    }
+
+    foreach ($glob in $item["dirs"]) {
+        $e_file = $item["exclude_file"]
+        $e_dir  = $item["exclude_item"]
+        if ($Load) {
+            $fromPath = Join-Path $storePath $glob["path"]
+            $toPath = Join-Path $localPath $glob["path"]
+        } else {
+            $toPath = Join-Path $storePath $glob["path"]
+            $fromPath = Join-Path $localPath $glob["path"]
+            if ($glob["exclude_file"]) {
+                $e_file += $glob["exclude_file"]
+            }
+            if ($glob["exclude_dir"]) {
+                $e_dir += $glob["exclude_dir"]
+            }
+        }
+        Write-Output "Copying from $fromPath to $toPath"
+        Write-Output "without [$($e_file -join ', ')] and [$e_dir]"
+        
+        # if (Test-Path "$fromDir") {
+        #         Remove-Item "$toDir" -Recurse -ErrorAction Ignore
+        #     if(-Not(Test-Path "$toDir")){
+        #         New-Item "$toDir" -ItemType Directory
+        #     }
+        #     robocopy "$fromDir" "$toDir" /mir
+        #     Write-Output "$fromDir copied to $toDir"
+        # }else {
+        #     Write-Output "$fromDir not found!"
+        # }
+
+    }
+}
 
 # foreach ($dir in $adSubdirs) {
 #     $toDir = Join-Path $localPath "$dir"
@@ -50,4 +85,4 @@ $cfg = psobj_to_hashmap($cfg)
 #     }
 # }
 
-#     Write-Output "Local: $($item.local)"
+#     Write-Output "Local: $($item["local"])"
