5568cd16f7896d5f7c71f512303f2cf61d34ee1e
Ddedalus <h.beres@hotmail.com>
2019-11-09 16:13:30 +0000

    Comments in the load file

--- load.bat
+++ load.bat
@@ -1,5 +1,10 @@
+:: install GitKraken
 START H:\Windows\GitKrakenSetup.exe
+
+:: copy user files according to specification in copy_list.json
 PowerShell  -NoProfile -ExecutionPolicy Bypass -File .\copy_from_list.ps1 copy_list.json -Load
+
+:: install VSCode extensions locally (too big to copy reliably)
 PowerShell -NoProfile -ExecutionPolicy Bypass -File H:\Windows\windows-profile\install_extensions.ps1
 
 :: add registry keys preventing Windows from searching the web in the Start menu
